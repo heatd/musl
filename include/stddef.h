2fa60de12e8b453b6cc402dd788405f3a9170e98
Pedro Falcato <pedro.falcato@gmail.com>
2022-12-21 04:52:57 +0000

    stddef.h: Make stddef.h C23 compliant
    Add unreachable(), nullptr_t and __STDC_VERSION_STDDEF_H__.
    
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- include/stddef.h
+++ include/stddef.h
@@ -1,6 +1,8 @@
 #ifndef _STDDEF_H
 #define _STDDEF_H
 
+#define __STDC_VERSION_STDDEF_H__ 202311L
+
 #if __cplusplus >= 201103L
 #define NULL nullptr
 #elif defined(__cplusplus)
@@ -22,6 +24,17 @@
 #define offsetof(type, member) __builtin_offsetof(type, member)
 #else
 #define offsetof(type, member) ((size_t)( (char *)&(((type *)0)->member) - (char *)0 ))
+#endif
+
+#if __STDC_VERSION__ >= 202311L
+
+// Unreachable macro - expands to a void expression that invokes UB if reached
+#define unreachable() __builtin_unreachable()
+
+#endif
+
+#if __STDC_VERSION__ >= 202311L && !defined(__cplusplus)
+typedef typeof(nullptr) nullptr_t;
 #endif
 
 #endif
