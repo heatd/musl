9ff8fa23381298791de4362b7d414179c7d4b688
Pedro Falcato <pedro.falcato@gmail.com>
2023-01-02 06:52:10 +0000

    riscv64: Fix ABI issues with struct stat
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- arch/riscv64/kstat.h
+++ arch/riscv64/kstat.h
@@ -1,21 +1,22 @@
 struct kstat {
 	dev_t st_dev;
 	ino_t st_ino;
-	mode_t st_mode;
 	nlink_t st_nlink;
+
+	mode_t st_mode;
 	uid_t st_uid;
 	gid_t st_gid;
+	unsigned int    __pad0;
 	dev_t st_rdev;
-	unsigned long __pad;
 	off_t st_size;
 	blksize_t st_blksize;
-	int __pad2;
 	blkcnt_t st_blocks;
+
 	long st_atime_sec;
 	long st_atime_nsec;
 	long st_mtime_sec;
 	long st_mtime_nsec;
 	long st_ctime_sec;
 	long st_ctime_nsec;
-	unsigned __unused[2];
+	long __unused[3];
 };
