1a7c1883cefdc886fd8224834f46cf5284581d37
Pedro Falcato <pedro.falcato@gmail.com>
2025-03-03 02:37:41 +0000

    Add x86 optimized string ops from bionic
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- src/string/x86_64/memset.s
+++ src/string/x86_64/memset.s
@@ -1,72 +1,120 @@
-.global memset
-.type memset,@function
-memset:
-	movzbq %sil,%rax
-	mov $0x101010101010101,%r8
-	imul %r8,%rax
+# 1 "src/string/x86_64/memset.S"
+# 1 "<built-in>" 1
+# 1 "src/string/x86_64/memset.S" 2
+# 26 "src/string/x86_64/memset.S"
+# 1 "src/string/x86_64/cache.h" 1
+# 27 "src/string/x86_64/memset.S" 2
+# 77 "src/string/x86_64/memset.S"
+ .section .text.avx2,"ax",@progbits
+# 90 "src/string/x86_64/memset.S"
+.type memset_avx2, @function; .globl memset_avx2; .p2align 4; memset_avx2: .cfi_startproc
+ movq %rdi, %rax
+ and $0xff, %rsi
+ mov $0x0101010101010101, %rcx
+ imul %rsi, %rcx
+ cmpq $16, %rdx
+ jae .L16bytesormore
+ testb $8, %dl
+ jnz .L8_15bytes
+ testb $4, %dl
+ jnz .L4_7bytes
+ testb $2, %dl
+ jnz .L2_3bytes
+ testb $1, %dl
+ jz .Lreturn
+ movb %cl, (%rdi)
+.Lreturn:
+ ret
 
-	cmp $126,%rdx
-	ja 2f
+.L8_15bytes:
+ movq %rcx, (%rdi)
+ movq %rcx, -8(%rdi, %rdx)
+ ret
 
-	test %edx,%edx
-	jz 1f
+.L4_7bytes:
+ movl %ecx, (%rdi)
+ movl %ecx, -4(%rdi, %rdx)
+ ret
 
-	mov %sil,(%rdi)
-	mov %sil,-1(%rdi,%rdx)
-	cmp $2,%edx
-	jbe 1f
+.L2_3bytes:
+ movw %cx, (%rdi)
+ movw %cx, -2(%rdi, %rdx)
+ ret
 
-	mov %ax,1(%rdi)
-	mov %ax,(-1-2)(%rdi,%rdx)
-	cmp $6,%edx
-	jbe 1f
+ .p2align 4
+.L16bytesormore:
+ movd %rcx, %xmm0
+ pshufd $0, %xmm0, %xmm0
+ movdqu %xmm0, (%rdi)
+ movdqu %xmm0, -16(%rdi, %rdx)
+ cmpq $32, %rdx
+ jbe .L32bytesless
+ movdqu %xmm0, 16(%rdi)
+ movdqu %xmm0, -32(%rdi, %rdx)
+ cmpq $64, %rdx
+ jbe .L64bytesless
+ movdqu %xmm0, 32(%rdi)
+ movdqu %xmm0, 48(%rdi)
+ movdqu %xmm0, -64(%rdi, %rdx)
+ movdqu %xmm0, -48(%rdi, %rdx)
+ cmpq $128, %rdx
+ jbe .L128bytesless
+        vpbroadcastb %xmm0, %ymm0
+ vmovdqu %ymm0, 64(%rdi)
+ vmovdqu %ymm0, 96(%rdi)
+ vmovdqu %ymm0, -128(%rdi, %rdx)
+ vmovdqu %ymm0, -96(%rdi, %rdx)
+ cmpq $256, %rdx
+        ja .L256bytesmore
+.L32bytesless:
+.L64bytesless:
+.L128bytesless:
+ ret
 
-	mov %eax,(1+2)(%rdi)
-	mov %eax,(-1-2-4)(%rdi,%rdx)
-	cmp $14,%edx
-	jbe 1f
+ .p2align 4
+.L256bytesmore:
+ leaq 128(%rdi), %rcx
+ andq $-128, %rcx
+ movq %rdx, %r8
+ addq %rdi, %rdx
+ andq $-128, %rdx
+ cmpq %rcx, %rdx
+ je .Lreturn
 
-	mov %rax,(1+2+4)(%rdi)
-	mov %rax,(-1-2-4-8)(%rdi,%rdx)
-	cmp $30,%edx
-	jbe 1f
 
-	mov %rax,(1+2+4+8)(%rdi)
-	mov %rax,(1+2+4+8+8)(%rdi)
-	mov %rax,(-1-2-4-8-16)(%rdi,%rdx)
-	mov %rax,(-1-2-4-8-8)(%rdi,%rdx)
-	cmp $62,%edx
-	jbe 1f
+ cmp $(4096*1024), %r8
 
-	mov %rax,(1+2+4+8+16)(%rdi)
-	mov %rax,(1+2+4+8+16+8)(%rdi)
-	mov %rax,(1+2+4+8+16+16)(%rdi)
-	mov %rax,(1+2+4+8+16+24)(%rdi)
-	mov %rax,(-1-2-4-8-16-32)(%rdi,%rdx)
-	mov %rax,(-1-2-4-8-16-24)(%rdi,%rdx)
-	mov %rax,(-1-2-4-8-16-16)(%rdi,%rdx)
-	mov %rax,(-1-2-4-8-16-8)(%rdi,%rdx)
 
-1:	mov %rdi,%rax
-	ret
 
-2:	test $15,%edi
-	mov %rdi,%r8
-	mov %rax,-8(%rdi,%rdx)
-	mov %rdx,%rcx
-	jnz 2f
+ ja .L256bytesmore_nt
 
-1:	shr $3,%rcx
-	rep
-	stosq
-	mov %r8,%rax
-	ret
+ .p2align 4
+.L256bytesmore_normal:
+ vmovdqa %ymm0, (%rcx)
+ vmovdqa %ymm0, 32(%rcx)
+ vmovdqa %ymm0, 64(%rcx)
+ vmovdqa %ymm0, 96(%rcx)
+ addq $128, %rcx
+ cmpq %rcx, %rdx
+ jne .L256bytesmore_normal
+ ret
 
-2:	xor %edx,%edx
-	sub %edi,%edx
-	and $15,%edx
-	mov %rax,(%rdi)
-	mov %rax,8(%rdi)
-	sub %rdx,%rcx
-	add %rdx,%rdi
-	jmp 1b
+ .p2align 4
+.L256bytesmore_nt:
+ movntdq %xmm0, (%rcx)
+ movntdq %xmm0, 16(%rcx)
+ movntdq %xmm0, 32(%rcx)
+ movntdq %xmm0, 48(%rcx)
+ movntdq %xmm0, 64(%rcx)
+ movntdq %xmm0, 80(%rcx)
+ movntdq %xmm0, 96(%rcx)
+ movntdq %xmm0, 112(%rcx)
+ leaq 128(%rcx), %rcx
+ cmpq %rcx, %rdx
+ jne .L256bytesmore_nt
+ sfence
+ ret
+
+.cfi_endproc; .size memset_avx2, .-memset_avx2
+
+.globl memset; .equ memset, memset_avx2
