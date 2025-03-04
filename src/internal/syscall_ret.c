9858689526fe6aa0f64cb6b0f6f281eea52f7b67
Pedro Falcato <pedro.falcato@gmail.com>
2025-03-04 01:40:23 +0000

    onyx: Add syscall_ret hack for older Onyx versions
    ...which don't do proper errno casting.
    
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- src/internal/syscall_ret.c
+++ src/internal/syscall_ret.c
@@ -3,7 +3,8 @@
 
 long __syscall_ret(unsigned long r)
 {
-	if (r > -4096UL) {
+	/* TODO: We need this cast because the kernel doesn't cast return values properly, just yet... */
+	if ((int) r > -4096UL) {
 		errno = -r;
 		return -1;
 	}
