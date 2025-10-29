fb6d4325f6236b752d26ee0d366db56adbe20cca
Pedro Falcato <pedro.falcato@gmail.com>
2025-10-29 17:11:50 +0000

    Revert "onyx: Add syscall_ret hack for older Onyx versions"
    This reverts commit 7d14c29360cf809a267eafb78f9af06fa2677cd2.
    This hack breaks large return values which aren't negative.

--- src/internal/syscall_ret.c
+++ src/internal/syscall_ret.c
@@ -3,8 +3,7 @@
 
 long __syscall_ret(unsigned long r)
 {
-	/* TODO: We need this cast because the kernel doesn't cast return values properly, just yet... */
-	if ((int) r > -4096UL) {
+	if (r > -4096UL) {
 		errno = -r;
 		return -1;
 	}
